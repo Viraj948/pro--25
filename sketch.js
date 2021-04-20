d67263a251968f2172ee1ac5053a429e82cbbf30
Viraj948 <74658897+Viraj948@users.noreply.github.com>
2021-04-20 16:27:05 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 const Render = Matter.Render;
-var dustbinObj,groundObject	,paper1
+var dustbinObj, paperObject,groundObject	
 var world;
 
 
@@ -16,11 +16,24 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 	
+	paperObject=new paper(200,450,70);
 	groundObject=new ground(width/2,670,width,20);
 	dustbinObj=new dustbin(1200,650);
+	//Create a Ground
 	
-    paper1=new Paper(200,350,50,50)
+
+	var render = Render.create({
+	  element: document.body,
+	  engine: engine,
+	  options: {
+	    width: 1600,
+	    height: 700,
+	    wireframes: false
+	  }
+	});
+
 	Engine.run(engine);
+	Render.run(render);
   
 }
 
@@ -28,15 +41,30 @@ function setup() {
 function draw() {
   rectMode(CENTER);
   background(230);
+ 
   
- keyPressed();
+  paperObject.display();
   groundObject.display();
   dustbinObj.display();
-  paper1.display();
+
+  
+  
+ 
+  
+  
+ 
 }
 
-function keyPressed(){
-	if (keyCode === UP_ARROW) {
-Matter.Body.applyForce(paper1.body,paper1.body.position,{x:30,y:-45});
- } 
+function keyPressed() {
+  	if (keyCode === UP_ARROW) {
+
+    	Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:130,y:-145});
+
+    
+  	}
 }
+
+
+
+
+
